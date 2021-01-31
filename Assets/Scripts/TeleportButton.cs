112852bfcc5970976d514e602f6330a596f1badf
RheaAyase <radka.janek@redhat.com>
2021-01-31 23:54:20 +0100

    init

---
+++ Assets/Scripts/TeleportButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRC.SDKBase;
+using UdonSharp;
+
+public class TeleportButton : UdonSharpBehaviour
+{
+    public Transform TargetPoint;
+
+    public override void Interact()
+    {
+        Networking.LocalPlayer.TeleportTo(this.TargetPoint.position, this.TargetPoint.rotation);
+        //base.Interact();
+    }
+}
---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonSharp;
+
+public class ToggleButton : UdonSharpBehaviour
+{
+    public GameObject TargetObject;
+
+    public override void Interact()
+    {
+        this.TargetObject.SetActive(!this.TargetObject.activeSelf);
+    }
+}
