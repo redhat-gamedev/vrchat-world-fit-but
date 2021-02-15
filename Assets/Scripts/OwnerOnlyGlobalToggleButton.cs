5fe7be21eff5405271c8466ee5342efbe78f4cdf
RheaAyase <radka.janek@redhat.com>
2021-02-15 12:26:00 +0100

    Added global toggles for the features, fixed lightmaps and event rules.

---
+++ Assets/Scripts/OwnerOnlyGlobalToggleButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonSharp;
+
+public class OwnerOnlyGlobalToggleButton : UdonSharpBehaviour
+{
+    public GameObject[] TargetObjects;
+
+    [UdonSynced]
+    public bool ToggleEnabled = false;
+
+    public override void Interact()
+    {
+        this.ToggleEnabled = !this.ToggleEnabled;
+        OnDeserialization();
+    }
+
+    public override void OnDeserialization()
+    {
+        foreach(GameObject go in this.TargetObjects)
+            go.SetActive(this.ToggleEnabled);
+    }
+}
